6a04e98ece1d36a7b28d8faf87098ec971db3128
Chaymae  Mechouahi <75322015+Cha1ymae@users.noreply.github.com>
2024-04-06 14:19:21 +0200

    Update header.jsx

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,35 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "../css/header.css";
 import { Link } from "react-router-dom";
-import useSearchBook from "../Hooks/useSearchBook";
 
 const Header = () => {
-  const [query, setQuery] = useState('');
-  const { books, isLoading } = useSearchBook(query);
-
-  const handleInputChange = (event) => {
-    setQuery(event.target.value);
-  };
-
   return (
     <header className="Header">
       <section>
         <h1 className="logo">BIBLIO</h1>
-        <div className="search-container">
-          <input
-            type="text"
-            placeholder="Rechercher..."
-            className="search-input"
-            value={query}
-            onChange={handleInputChange}
-          />
-          <button id="loupe" type="submit">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSarYxIAzBON-FZiNHYbvdW4HKuIYpdKGtKRA&usqp=CAU"
-              alt="Loupe"
-            />
-          </button>
-        </div>
         <menu>
           <Link to="/home">
             <button>Home</button>
@@ -44,21 +21,6 @@ const Header = () => {
           </Link>
         </menu>
       </section>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          <p>Number of books found: {books.length}</p>
-          <ul>
-            {books.map((book, index) => (
-              <li key={index}>
-                <Link to={`/book/${index}`}>{book.title}</Link>
-              </li>
-            ))}
-          </ul>
-          {books.length > 0 && <p>Author Name: {books[0].author_name}</p>}
-        </div>
-      )}
     </header>
   );
 };
