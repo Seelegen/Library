3ce0176b43a9b3adda3de532d31f6934a10a69d5
Chaymae Mechouahi <chaymaemechouahi@gmail.com>
2024-04-21 13:22:26 +0200

    CMI:FinirMaPartieDeProjet

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,23 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/header.css";
 import { Link } from "react-router-dom";
+import useSearchOneBook from "../Hooks/useSearchOneBook";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const searchResultsOneBook = useSearchOneBook(searchQuery);
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleBookClick = () => {
+    setSearchQuery("");
+  };
+
   return (
     <header className="Header">
-      <section>
-        <h1 className="logo">BIBLIO</h1>
-        <menu>
-
-          <Link to="/book">
-            <button>Livre</button>
-          </Link>
-
-          <Link to="/author">
-            <button>Auteur</button>
-          </Link>
-        </menu>
-      </section>
+      <div className="logo">BIBLIO</div>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Rechercher un livre..."
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+        <ul className="search-results">
+          {searchResultsOneBook.map((book, index) => (
+            <li key={index}>
+              <Link
+                to={`/book/${encodeURIComponent(book.title)}`}
+                onClick={handleBookClick} 
+              >
+                {book.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <nav>
+        <Link to="/book">
+          <button>Livre</button>
+        </Link>
+        <Link to="/author">
+          <button>Auteur</button>
+        </Link>
+      </nav>
     </header>
   );
 };
