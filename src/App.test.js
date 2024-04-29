527f3e13eec08e3bcebafc0d55bd61eaa4b8c47d
Seelegen <martin.dlfg@gmail.com>
2024-04-29 19:31:03 +0200

    Last Change check Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
@@ -10,4 +10,24 @@ test('renders "Loading" text on homepage', () => {
   );
   const loadingText = screen.getByText(/Loading/i);
   expect(loadingText).toBeInTheDocument();
+});
+
+test('renders "Derniers changements" text after loading', async () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+
+  await waitFor(() => {
+    const loadingText = screen.queryByText(/Loading/i);
+    return !loadingText;
+  });
+
+  // Attendre 3 secondes
+  await new Promise(resolve => setTimeout(resolve, 3000));
+
+  // Vérifier si le texte "Derniers changements" est affiché après l'attente de 3 secondes
+  const derniersChangementsText = screen.getByText(/Derniers changements/i);
+  expect(derniersChangementsText).toBeInTheDocument();
 });
