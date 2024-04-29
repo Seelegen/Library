4a985dd644a1a52f939110047baf031529f396ee
Seelegen <martin.dlfg@gmail.com>
2024-04-29 19:20:06 +0200

    Test for Loading

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders "Loading" text on homepage', () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  const loadingText = screen.getByText(/Loading/i);
+  expect(loadingText).toBeInTheDocument();
 });
